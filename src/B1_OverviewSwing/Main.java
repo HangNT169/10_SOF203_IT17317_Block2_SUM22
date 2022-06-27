045857688f0143ff81defc7784722f67c3f746e6
HangNT164 <50843516+HangNT164@users.noreply.github.com>
2022-06-27 15:47:48 +0700

    B1_Overview Swing

---
+++ src/B1_OverviewSwing/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B1_OverviewSwing;
+
+/**
+ *
+ * @author hangnt
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SinhVien sv = new SinhVien();
+//        sv.setTen("aaa");
+//        sv.setLoai("aaa");
+//SinhVien sv = new SinhVien(ten, loai, true, soThich);
+    }
+}
